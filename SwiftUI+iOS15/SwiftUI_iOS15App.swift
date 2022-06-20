08d143a4ca4a24d4f3629bcfbf9bc54fbbbd9583
pst <skunow2@naver.com>
2022-06-20 21:54:07 +0900

    create project

---
+++ SwiftUI+iOS15/SwiftUI_iOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_iOS15App.swift
+//  SwiftUI+iOS15
+//
+//  Created by 박승태 on 2022/06/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_iOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
