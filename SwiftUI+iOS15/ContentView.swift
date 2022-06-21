476815fc0f072ed51ccaa9abbd7450df81687b2c
pst <skunow2@naver.com>
2022-06-21 22:19:54 +0900

    complete chapter3

--- SwiftUI+iOS15/ContentView.swift
+++ SwiftUI+iOS15/ContentView.swift
@@ -10,35 +10,43 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack(alignment: .leading,
-               spacing: 8.0) {
+        VStack(alignment: .leading, spacing: 8.0) {
             Spacer()
             Image("Logo 2")
                 .resizable(resizingMode: .stretch)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
-                .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+                .cornerRadius(20.0)
             Text("SwiftUI for iOS 15")
                 .font(.largeTitle)
-            .fontWeight(.bold)
-            Text("20 sections - 3 hours")
+                .fontWeight(.bold)
+            Text("20 sections - 3 hours".uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
+                .foregroundColor(.secondary)
             Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
         }
-               .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
-               .frame(height: 350.0)
-               .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("Background")/*@END_MENU_TOKEN@*/)
-               .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
-               .shadow(radius: /*@START_MENU_TOKEN@*/20/*@END_MENU_TOKEN@*/)
+        .padding(.all, 20.0)
+        .padding(.vertical, 20)
+        .frame(height: 350.0)
+        .background(Color("Background"))
+        .cornerRadius(30.0)
+        .shadow(color: Color("Shadow").opacity(0.3),
+                radius: 10, x: 0, y: 10)
+        .padding(.horizontal, 20)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .previewDevice("iPhone 12")
     }
 }
