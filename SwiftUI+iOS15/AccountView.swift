857db0d3cbbe858b5ed20db3a3e5f3819e024aef
pst <skunow2@naver.com>
2022-06-27 20:54:36 +0900

    chapter11 - Structure and Toggle

--- SwiftUI+iOS15/AccountView.swift
+++ SwiftUI+iOS15/AccountView.swift
@@ -9,90 +9,115 @@ import SwiftUI
 
 struct AccountView: View {
     @State var isDeleted: Bool = false
+    @State var isPinned: Bool = false
     
     var body: some View {
         NavigationView {
             List {
-                VStack(spacing: 8) {
-                    Image(systemName: "person.crop.circle.fill.badge.checkmark")
-                        .symbolVariant(.circle.fill)
-                        .font(.system(size: 32))
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.blue, .blue.opacity(0.3))
-                        .padding()
-                        .background(Circle().fill(.ultraThinMaterial))
-                        .background(
-                            Image(systemName: "hexagon")
-                                .symbolVariant(.fill)
-                                .foregroundColor(.blue)
-                                .font(.system(size: 200))
-                                .offset(x: -50, y: -100)
-                        )
-                    Text("박승태")
-                        .font(.title.weight(.semibold))
-                    HStack {
-                        Image(systemName: "location")
-                            .imageScale(.large)
-                        Text("부천")
-                            .foregroundColor(.secondary)
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                .padding()
+                self.profile
                 
-                Section {
-                    NavigationLink(destination: ContentView()) {
-                        Label("Settings", systemImage: "gear")
-                            .accentColor(.primary)
-                    }
-                    NavigationLink { Text("Billing") } label: {
-                        Label("Billing", systemImage: "creditcard")
-                    }
-                    NavigationLink { ContentView()} label: {
-                        Label("Help", systemImage: "questionmark")
-                            .imageScale(.small)
-                    }
-                }
-                .accentColor(.primary)
-                .listRowSeparatorTint(.blue)
-                .listRowSeparator(.hidden)
+                self.menu
                 
-                Section {
-                    if !self.isDeleted {
-                        Link(destination: URL(string: "https://www.apple.com")!) {
-                            HStack {
-                                Label("Apple", systemImage: "house")
-                                Spacer()
-                                Image(systemName: "link")
-                                    .foregroundColor(.secondary)
-                            }
-                        }
-                        .swipeActions(edge: .leading,
-                                      allowsFullSwipe: false) {
-                            Button(action: { self.isDeleted = true }) {
-                                Label("Delete", systemImage: "trash")
-                            }
-                            .tint(.red)
-                            Button { self.isDeleted = false } label: {
-                                Label("Pin", systemImage: "pin")
-                            }
-                            .tint(.yellow)
-                        }
-                    }
-                    Link(destination: URL(string: "https://www.naver.com")!) {
-                        HStack {
-                            Label("Naver", systemImage: "tv")
-                            Spacer()
-                            Image(systemName: "link")
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                }
-                .accentColor(.primary)
+                self.links
             }
             .listStyle(.insetGrouped)
             .navigationTitle("Account")
         }
+    }
+    
+    var profile: some View {
+        VStack(spacing: 8) {
+            Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                .symbolVariant(.circle.fill)
+                .font(.system(size: 32))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.blue, .blue.opacity(0.3))
+                .padding()
+                .background(Circle().fill(.ultraThinMaterial))
+                .background(
+                    Image(systemName: "hexagon")
+                        .symbolVariant(.fill)
+                        .foregroundColor(.blue)
+                        .font(.system(size: 200))
+                        .offset(x: -50, y: -100)
+                )
+            Text("박승태")
+                .font(.title.weight(.semibold))
+            HStack {
+                Image(systemName: "location")
+                    .imageScale(.large)
+                Text("부천")
+                    .foregroundColor(.secondary)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+    }
+    
+    var menu: some View {
+        Section {
+            NavigationLink(destination: ContentView()) {
+                Label("Settings", systemImage: "gear")
+                    .accentColor(.primary)
+            }
+            NavigationLink { Text("Billing") } label: {
+                Label("Billing", systemImage: "creditcard")
+            }
+            NavigationLink { ContentView()} label: {
+                Label("Help", systemImage: "questionmark")
+                    .imageScale(.small)
+            }
+        }
+        .accentColor(.primary)
+        .listRowSeparatorTint(.blue)
+        .listRowSeparator(.hidden)
+    }
+    
+    var links: some View {
+        Section {
+            if !self.isDeleted {
+                Link(destination: URL(string: "https://www.apple.com")!) {
+                    HStack {
+                        Label("Apple", systemImage: "house")
+                        Spacer()
+                        Image(systemName: "link")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .swipeActions(edge: .leading,
+                              allowsFullSwipe: false) {
+                    Button(action: { self.isDeleted = true }) {
+                        Label("Delete", systemImage: "trash")
+                    }
+                    .tint(.red)
+                    self.pinButton
+                }
+            }
+            Link(destination: URL(string: "https://www.naver.com")!) {
+                HStack {
+                    Label("Naver", systemImage: "tv")
+                    Spacer()
+                    Image(systemName: "link")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .swipeActions {
+                self.pinButton
+            }
+        }
+        .accentColor(.primary)
+    }
+    
+    var pinButton: some View {
+        Button { self.isPinned.toggle() } label: {
+            if self.isPinned {
+                Label("Unpin", systemImage: "pin.slash")
+            }
+            else {
+                Label("Pin", systemImage: "pin")
+            }
+        }
+        .tint(self.isPinned ? .gray : .yellow)
     }
 }
 
