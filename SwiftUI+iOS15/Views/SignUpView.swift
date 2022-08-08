3e6fb97a32c28ba6c39918f3d3fc1f8c2560b073
pst <skunow2@naver.com>
2022-08-08 22:19:22 +0900

    chapter49 - Field Circle Animation

--- SwiftUI+iOS15/Views/SignUpView.swift
+++ SwiftUI+iOS15/Views/SignUpView.swift
@@ -17,6 +17,9 @@ struct SignUpView: View {
     @State var password: String = ""
     @FocusState var focusedField: Field?
     @State var circleY: CGFloat = 120
+    @State var emailY: CGFloat = 0
+    @State var passwordY: CGFloat = 0
+    @State var circleColor: Color = .blue
     
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
@@ -32,11 +35,20 @@ struct SignUpView: View {
                 .disableAutocorrection(true)
                 .focused($focusedField, equals: .email)
                 .shadow(color: self.focusedField == .email ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
+                .overlay(self.geometry)
+                .onPreferenceChange(CirclePreferenceKey.self) { value in
+                    self.emailY = value
+                    self.circleY = value
+                }
             SecureField("Password", text: $password)
                 .inputStyle(icon: "lock")
                 .textContentType(.password)
                 .focused($focusedField, equals: .password)
                 .shadow(color: self.focusedField == .password ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
+                .overlay(self.geometry)
+                .onPreferenceChange(CirclePreferenceKey.self) { value in
+                    self.passwordY = value
+                }
             Button { } label: {
                 Text("Create an account")
                     .frame(maxWidth: .infinity)
@@ -68,11 +80,12 @@ struct SignUpView: View {
         .padding(20)
         .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
         .background(
-            Circle().fill(.blue)
+            Circle().fill(self.circleColor)
                 .frame(width: 68, height: 68)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
                 .offset(y: self.circleY)
         )
+        .coordinateSpace(name: "container")
         .strokeStyle(cornerRadius: 30)
         .shadow(color: Color("Shadow").opacity(0.2), radius: 30, x: 0, y: 30)
         .padding(20)
@@ -81,8 +94,15 @@ struct SignUpView: View {
         )
         .onChange(of: self.focusedField) { value in
             withAnimation {
-                self.circleY = value == .email ? 120 : 190
+                self.circleY = value == .email ? self.emailY : self.passwordY
+                self.circleColor = value == .email ? .blue : .red
             }
+        }
+    }
+    
+    var geometry: some View {
+        GeometryReader { proxy in
+            Color.clear.preference(key: CirclePreferenceKey.self, value: proxy.frame(in: .named("container")).minY)
         }
     }
 }
